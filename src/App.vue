ac95b12d6f34aff0189daf5c33e6c681dc91d574
Baskinsen <92514412+Baskinsen@users.noreply.github.com>
2024-04-11 22:26:44 +0100

    header with responsiveness

---
+++ src/App.vue
@@ -0,0 +1,23 @@
+<script setup>
+import Header from './components/Header.vue';
+</script>
+
+<template>
+  <Header />
+</template>
+
+<style scoped>
+button {
+  padding: 10px 30px;
+  background-color: #008000;
+  border: none;
+  color: #ffffff;
+  font-size: 18px;
+  border-radius: 5px;
+}
+
+button a {
+  color: #ffffff;
+
+}
+</style>
